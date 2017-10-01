5f5ac87e63e06a723ed9b5f92b7710cb7c19b544
Nick Papoutsis <izybit@gmail.com>
2017-10-01 19:02:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function updateObjectWithKeyAndValue(obj, key, value){
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
 
-  let x = Object.assign(obj, key, value);
-  return x;
+  obj[key] = value;
+  return obj;
 }
