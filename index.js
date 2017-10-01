76af6e6bb6fb72e7242328afaf7572d1773d9672
Nick Papoutsis <izybit@gmail.com>
2017-10-01 19:05:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ function updateObjectWithKeyAndValue(obj, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
-
   obj[key] = value;
+  return obj;
+}
+
+function deleteFromObjectByKey(obj, key){
+  delete obj[key];
   return obj;
 }
